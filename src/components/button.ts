9a8683538774c21348fbd776959ee5e1f93027e7
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-09-08 01:44:34 +0900

    create dist branch to deploy

--- src/components/button.ts
+++
@@ -1,9 +0,0 @@
-export class Button {
-  el
-  constructor(parent:HTMLElement, content: string) {
-    this.el = document.createElement('button');
-    this.el.className = 'button';
-    this.el.textContent = content;
-    parent.append(this.el);
-  }
-}
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import './style.css';
-import { App } from "./components/app";
-
-const app = new App(document.body, 'main-wrap');
